343c00d9e8691e65aa014c02ca30d7da1916a53c
Davi Cecilio <davim.cecilio@gmail.com>
2021-05-25 14:27:25 -0300

    feat(product-create): add form

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 
+import { FormsModule } from "@angular/forms"
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
@@ -9,6 +10,9 @@ import { MatListModule } from "@angular/material/list"
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button'
 import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatInputModule } from "@angular/material/input"
+
 
 import { AppRoutingModule } from '@/app-routing.module'
 import { AppComponent } from '@/app.component'
@@ -49,7 +53,10 @@ import { HttpClientModule } from "@angular/common/http"
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
