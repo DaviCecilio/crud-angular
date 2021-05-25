372ec84a9dd83df582830321fc393ae6a394cae7
Davi Cecilio <davim.cecilio@gmail.com>
2021-05-25 10:36:13 -0300

    feat(views): create home component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { HeaderComponent } from './components/templates/header/header.component'
-
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { FooterComponent } from './components/templates/footer/footer.component'
-import { NavComponent } from './components/templates/nav/nav.component'
-
 import { MatSidenavModule } from "@angular/material/sidenav"
+import { MatCardModule } from "@angular/material/card"
 import { MatListModule } from "@angular/material/list"
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { AppRoutingModule } from '@/app-routing.module'
+import { AppComponent } from '@/app.component'
+
+import { HeaderComponent } from '@/components/templates/header/header.component'
+import { FooterComponent } from "@/components/templates/footer/footer.component"
+import { NavComponent } from "@/components/templates/nav/nav.component"
+import { HomeComponent } from '@/views/home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +28,8 @@ import { MatListModule } from "@angular/material/list"
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/templates/nav/nav.component.ts
+++ frontend/src/app/components/templates/nav/nav.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
+//Decorator
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor () { }
 
   ngOnInit(): void {
   }
