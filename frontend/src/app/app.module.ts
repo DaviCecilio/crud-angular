8b0eb758444a2b78e0ade66fe7a9a77408ae40c6
Davi Cecilio <davim.cecilio@gmail.com>
2021-05-25 11:18:00 -0300

    feat(components): create product-create

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,8 +13,10 @@ import { HeaderComponent } from '@/components/templates/header/header.component'
 import { FooterComponent } from "@/components/templates/footer/footer.component"
 import { NavComponent } from "@/components/templates/nav/nav.component"
 import { HomeComponent } from '@/views/home/home.component'
-import { ProductCrudComponent } from '@/views/product-crud/product-crud.component';
+import { ProductCrudComponent } from '@/views/product-crud/product-crud.component'
 import { RedDirective } from './directives/red.directive'
+import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './components/products/product-create/product-create.component'
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { RedDirective } from './directives/red.directive'
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    RedDirective
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
